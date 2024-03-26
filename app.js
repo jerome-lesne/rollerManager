75636233a98b7de4196c21a0abbbdecdbb1aea53
Jerome <jeromelesne@gmail.com>
2024-03-26 16:58:51 +0100

    server initialized

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const session = require("express-session");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use(express.static("./public"));
+app.use(express.urlencoded({ extended: true }));
+
+app.use(
+    session({
+        secret: process.env.secretKey,
+        resave: true,
+        saveUninitialized: true,
+    }),
+);
+
+app.listen(process.env.PORT, (e) => {
+    if (e) {
+        console.log(e);
+    } else {
+        console.log(`server connected on PORT: ${process.env.PORT}`);
+    }
+});
+
+mongoose.connect(process.env.dbURI);
