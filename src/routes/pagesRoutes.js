ba8b9484c692e01adb8f16563bb4021c0f1337d7
Jerome <jeromelesne@gmail.com>
2024-04-19 17:51:24 +0200

    modified authguard to add role parameter in order to protect routes where user do not have enought privileges (roles related)

--- src/routes/pagesRoutes.js
+++ src/routes/pagesRoutes.js
@@ -12,8 +12,8 @@ const authguard = require("../services/authguardService");
 pagesRouter.get("/", home);
 pagesRouter.get("/login", login);
 pagesRouter.get("/try", tryReq);
-pagesRouter.get("/dashboard", authguard, dashboard);
-pagesRouter.get("/management", management);
+pagesRouter.get("/dashboard", authguard(), dashboard);
+pagesRouter.get("/management", authguard("bureau"), management);
 pagesRouter.get("/subscribe/:token", subscribe);
 
 module.exports = pagesRouter;
--- src/services/authguardService.js
+++ src/services/authguardService.js
@@ -1,20 +1,26 @@
 const membersModel = require("../models/membersModel");
 
-const authguard = async (req, res, next) => {
-    try {
-        if (req.session.memberId) {
-            let member = await membersModel.findOne({
-                _id: req.session.memberId,
-            });
-            if (member) {
-                return next();
+const authguard = (role = undefined) => {
+    return async (req, res, next) => {
+        try {
+            if (req.session.memberId) {
+                let member = await membersModel.findOne({
+                    _id: req.session.memberId,
+                });
+                if (member) {
+                    if (!role) {
+                        return next();
+                    } else if (member.role.includes(role)) {
+                        return next();
+                    }
+                }
             }
+            throw new Error("no access granted");
+        } catch (e) {
+            console.log(e.message);
+            res.status(401).render("login/index.html.twig");
         }
-        throw new Error("member not connected");
-    } catch (e) {
-        console.log(e.message);
-        res.status(401).render("login/index.html.twig");
-    }
+    };
 };
 
 module.exports = authguard;
