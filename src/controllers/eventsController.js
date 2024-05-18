7a631998c26feb9ae231a8532b61278a3c059ecb
Jerome <jeromelesne@gmail.com>
2024-05-18 14:45:39 +0200

    fill the start and end date and time on an event when timeslot selected

--- src/controllers/eventsController.js
+++ src/controllers/eventsController.js
@@ -1,9 +1,15 @@
 const getEventForm = async (req, res) => {
     try {
         if (req.body.eventType === "training") {
-            res.render("calendar/_newTrainingForm.html.twig");
+            res.render("calendar/_newTrainingForm.html.twig", {
+                start_select: req.body.start_select,
+                end_select: req.body.end_select,
+            });
         } else if (req.body.eventType === "match") {
-            res.render("calendar/_newMatchForm.html.twig");
+            res.render("calendar/_newMatchForm.html.twig", {
+                start_select: req.body.start_select,
+                end_select: req.body.end_select,
+            });
         } else {
             res.status(500).send("server error");
         }
@@ -20,4 +26,13 @@ const cancelCreateEvent = async (req, res) => {
     }
 };
 
-module.exports = { getEventForm, cancelCreateEvent };
+const addTraining = async (req, res) => {
+    try {
+        console.log(req.body);
+        res.render("calendar/_newEventForm.html.twig");
+    } catch (e) {
+        res.status(500).send("server error");
+    }
+};
+
+module.exports = { getEventForm, cancelCreateEvent, addTraining };
--- src/routes/eventsRoutes.js
+++ src/routes/eventsRoutes.js
@@ -2,9 +2,11 @@ const eventsRouter = require("express").Router();
 const {
     getEventForm,
     cancelCreateEvent,
+    addTraining,
 } = require("../controllers/eventsController");
 
 eventsRouter.post("/get-event-form", getEventForm);
 eventsRouter.get("/cancel-create-event", cancelCreateEvent);
+eventsRouter.post("/add-training", addTraining);
 
 module.exports = eventsRouter;
